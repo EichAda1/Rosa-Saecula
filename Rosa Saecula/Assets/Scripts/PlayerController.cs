8868248b302db776bf99fc483715047d1848304b
EichAda1 <adameichenberg@gmail.com>
2023-09-22 15:29:14 -0400

    More Player Controller and Camera

--- Rosa Saecula/Assets/Scripts/PlayerController.cs
+++ Rosa Saecula/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkSpeed = 1;
     private float xAxis;
 
+    [SerializeField]private float jumpForce = 45;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckX = 0.2f;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private LayerMask isGround;
+
+    Animator anim;
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            Instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +42,8 @@ public class PlayerController : MonoBehaviour
     {
         Inputs();
         Move();
+        Jump();
+        Flip();
     }
 
     void Inputs()
@@ -30,5 +54,44 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+    }
+
+    public bool isGrounded()
+    {
+        if(Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckY, isGround) 
+            || Physics2D.Raycast(groundCheck.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, isGround) 
+            || Physics2D.Raycast(groundCheck.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, isGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void Flip()
+    {
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+        else if(xAxis > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
     }
 }
