62c4022f2519d45592f9a054bad95fe9ca4eb55f
ZVandenToorn <141524480+ZVandenToorn@users.noreply.github.com>
2023-09-23 20:03:06 -0400

    Starting room added, along with empty scripts that are needed.

--- Rosa Saecula/Assets/Scripts/CameraFollow.cs
+++ Rosa Saecula/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,11 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private float  followSpeed = 0.1f;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private float yMaxBounds;
+    [SerializeField] private float yMinBounds;
+    [SerializeField] private float xMaxBounds;
+    [SerializeField] private float xMinBounds;
+    
 
     // Start is called before the first frame update
     void Start()
